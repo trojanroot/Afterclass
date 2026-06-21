6ee8ab57468237777d4e52d5b7a8993fda94476a
trojanroot <thapaaryan926@gmail.com>
2026-06-21 17:58:35 +0545

    return type with arrow function

--- day6/script.js
+++ day6/script.js
@@ -3,3 +3,23 @@ function sayName(){
     return name;
 }
 console.log(sayName());
+
+//return type
+function name(){
+    return "aryan";
+}
+let hold=name();
+console.log(hold);
+
+
+
+const num=a=>{
+    if(a%2==0){
+        return "even";
+    }
+    else{
+        return "odd";
+    }
+}
+hold=num(7);
+console.log(hold);
