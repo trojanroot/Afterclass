b10cb5af2965073865c3ee9649b10ec0046f5c8f
trojanroot <thapaaryan926@gmail.com>
2026-07-04 08:31:47 +0545

    done task of multi array using for each in it

--- day10/script.js
+++ day10/script.js
@@ -9,7 +9,7 @@ const usingforEach=()=>{
         console.log("Using Arrow Function ",element);
     });
 };
-usingforEach();
+//usingforEach();
 
 const listofLaptop=()=>{
     let laptop=["lenovo","Acer","Macbook","Dell","HP"];
@@ -18,7 +18,7 @@ const listofLaptop=()=>{
     })
 }
 
-listofLaptop();
+//listofLaptop();
 
 const listofMobiles=()=>{
     let mobile=["SAMSUNG","iphone","Redmi","VIVO","Realme"];
@@ -26,7 +26,7 @@ const listofMobiles=()=>{
         console.log(i);
     });
 };
-listofMobiles();
+//listofMobiles();
 
 const listofFruits=()=>{
     let fruits=["apple","banana","orange","mango","pineapple","watermelon","litchi","strawberry","guava","grapes"];
@@ -34,8 +34,9 @@ const listofFruits=()=>{
         console.log("list of fruits are: ",elements);
     });
 };
-listofFruits();
+//listofFruits();
 
+//list of 5 product like id 1
 const listofProduct=()=>{
     let product=[
         {
@@ -45,7 +46,7 @@ const listofProduct=()=>{
             price:2999,
             discountedPrice:2249,
             inStock:true,
-            description:"",
+            description:"An elegant ivory gold kurti designed for grateful everyday styling and specific occasions. The soft kinen blend  gives a breathable and lightweight feel, when the minimal thnnic finish adds a premimum Nepali clothing touch . perfect for office ,casual outing , family gathering and festival moments",
             size:{
                 small: "S",
                 medium: "M",
@@ -64,16 +65,107 @@ const listofProduct=()=>{
                 fit1:"straightFit",
                 fit2:"RegularFit",
             },
+        },
+        {
+            id:2,
+            name:"Women Cotton Slub Kurti-Mustard Yellow",
+            rating:"3.5star",
+            price:2599,
+            discountedPrice:2049,
+            inStock:true,
+            description:"A vibrant mustard yellow cotton slub kurti designed with a clean silhouette and fine detailing. Soft, lightweight, and breathable, it's an ideal choice for everyday comfort with a stylish finish.",
+            size:["s","m","l","xl"],
+            color:[
+                "Dusty Rose",
+            ],
+            facbric:"cotton slub",
+            fit:[
+                "straightFit",
+                "RegularFit",
+            ],
+        },
+        {
+            id:3,
+            name:"Women Cotton Kurti - Sage Green",
+            rating:"3star",
+            price:2499,
+            discountedPrice:1999,
+            inStock:true,
+            description:"A refreshing sage green cotton kurti designed for effortless everyday elegance. Crafted with breathable fabric and subtle embroidery, it offers comfort and sophistication for office wear, casual outings, and festive gatherings.",
+            size:["s","l","xl"],
+            color:[
+                "Wine Maroon",
+                "Charcoal Grey",
+                "Blush Pink",
+            ],
+            facbric:"100% Premium Cotton",
+            fit:[
+                "straightFit",
+                "RegularFit",
+            ],
+        },
+        {
+                id:4,
+            name:"Women Linen Kurti - Blush Pink",
+            rating:"4.5star",
+            price:2599,
+            discountedPrice:2249,
+            inStock:false,
+            description:"An elegant blush pink linen kurti tailored for modern women who appreciate timeless fashion. The breathable linen blend provides a luxurious feel, making it perfect for work, brunches, and special occasions.",
+            size:["s","m","l","xl"],
+            color:[
+                "Emerald Green",
+                "Pearl White",
+            ],
+            facbric:"linen brand",
+            fit:[
+                "Straight fit",
+            ],
+        },
+        {
+                id:5,
+            name:"Women Viscose Kurti-Lavender Mist",
+            rating:"4star",
+            price:2799,
+            discountedPrice:2399,
+            inStock:true,
+            description:"A sophisticated lavender mist viscose kurti that combines elegance with comfort. Its silky-soft texture and contemporary styling make it suitable for office wear, casual meetups, and evening outings.",
+            size:{
+                small:"s",
+                meduium:"m",
+                large:"l",
+                extraLarge:"xl"
+            },
+            color:{
+                color1:"Rust Orange",
+                color2:"Teal Blue",
+                color3:"Mocha Brown",
+                color4:"Olive Green",
+            },
+            facbric:"linen brand",
+            fit:{
+               fit1:"Straight fit",}
+            ,
         }
     ];
-    console.log(product);
+    // console.log(product);
     product.forEach((elements)=>{
+        console.log("    ");
         console.log(elements.id);
         console.log(elements.name);
         console.log(elements.rating);
         console.log(elements.price);
+        console.log(elements.discountedPrice);
         console.log(elements.inStock);
+        console.log(elements.description);
+        console.log(elements.size);
+        console.log(elements.color);
+        console.log(elements.facbric);
+        console.log(elements.fit);
+        console.log("");
         console.log("---------------------------");
+
+
     })
     
 }
